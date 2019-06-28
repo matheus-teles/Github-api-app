907aba7d8b30419aaa7bf83cb79029b31c872260
Matheus Teles <teles.matheusm@gmail.com>
2019-06-28 16:14:34 -0300

    remove unnecessary action

--- redux/actions/users.js
+++ redux/actions/users.js
@@ -13,18 +13,6 @@ export function receiveUsers(json) {
   }
 }
 
-
-export function fetchUsers() {
-  return (dispatch, getState) => {
-    const users = getState().users.items;
-    const lastSeenId = users.length ? users[users.length - 1].id : 0
-    dispatch(requestUsers())
-    return fetch(`https://api.github.com/users?since=${lastSeenId}`)
-      .then(response => response.json())
-      .then(json => dispatch(receiveUsers(json)))
-  }
-}
-
 export function searchUsers(query) {
   return dispatch => {
     dispatch(requestUsers())
